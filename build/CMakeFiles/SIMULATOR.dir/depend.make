3d6934d5c27714bac6a87d4306c0c937cfc72ba8
yuribomr <yuribomr@gmail.com>
2026-04-27 18:47:19 -0300

    Build removida

--- build/CMakeFiles/SIMULATOR.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SIMULATOR.
-# This may be replaced when dependencies are built.
--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuri/testes/raylib_project/colision-2d/build/_deps/raylib-src/src
-# Build directory: /home/yuri/testes/raylib_project/colision-2d/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
